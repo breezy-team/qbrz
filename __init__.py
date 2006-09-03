057a2cd59084998605558006e28f22bc3b69c32e
Lukáš Lalinský <lalinsky@gmail.com>
2006-09-03 10:58:09 +0200

    Absolute imports.

--- __init__.py
+++ __init__.py
@@ -17,9 +17,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-from commit import *
+from bzrlib.plugins.qbzr.commit import *
 
 from PyQt4 import QtGui
-import resources
+import bzrlib.plugins.qbzr.resources
 app = QtGui.QApplication(sys.argv)
 
