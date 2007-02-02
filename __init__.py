28d477ff187ab4401c46eb9d7de07a05c48fbb11
Lukáš Lalinský <lalinsky@gmail.com>
2007-02-02 13:48:12 +0100

    Bump the version number.

--- __init__.py
+++ __init__.py
@@ -17,6 +17,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
+__version__ = "0.2.0"
+
+
 import bzrlib.plugins.qbzr.resources
 import sys
 from bzrlib.option import Option
