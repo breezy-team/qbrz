e8ca583de002d22ea87cedd0418544c7ffdff1cb
Lukáš Lalinsky <lalinsky@gmail.com>
2007-09-06 09:40:11 +0200

    Update NEWS.txt and version numbers.

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ from distutils.core import setup
 setup(name='qbzr',
       description='Qt4 frontend for Bazaar',
       keywords='plugin bzr qt qbzr',
-      version='0.5.0',
-      url='http://lukas.oxygene.sk/wiki/QBzr',
+      version='0.6.0',
+      url='http://bazaar-vcs.org/QBzr',
       license='GPL',
       author='Lukáš Lalinský',
       author_email='lalinsky@gmail.com',
-      package_dir={'bzrlib.plugins.qbzr':'.'},
+      package_dir={'bzrlib.plugins.qbzr': '.'},
       packages=['bzrlib.plugins.qbzr']
       )
