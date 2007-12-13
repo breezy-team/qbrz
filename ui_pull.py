b9a9846041e8fdf76b54c668f901215b0b3c410a
Lukáš Lalinský <lalinsky@gmail.com>
2007-12-13 19:38:35 +0100

    Add compiled UI files.

---
+++ ui_pull.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui/pull.ui'
+#
+# Created: Thu Dec 13 18:42:13 2007
+#      by: PyQt4 UI code generator 4.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+from bzrlib.plugins.qbzr.i18n import gettext
+
+class Ui_PullForm(object):
+    def setupUi(self, PullForm):
+        PullForm.setObjectName("PullForm")
+        PullForm.resize(QtCore.QSize(QtCore.QRect(0,0,382,340).size()).expandedTo(PullForm.minimumSizeHint()))
+
+        self.vboxlayout = QtGui.QVBoxLayout(PullForm)
+        self.vboxlayout.setObjectName("vboxlayout")
+
+        self.groupBox = QtGui.QGroupBox(PullForm)
+        self.groupBox.setObjectName("groupBox")
+
+        self.gridlayout = QtGui.QGridLayout(self.groupBox)
+        self.gridlayout.setObjectName("gridlayout")
+
+        self.label_2 = QtGui.QLabel(self.groupBox)
+        self.label_2.setObjectName("label_2")
+        self.gridlayout.addWidget(self.label_2,0,0,1,1)
+
+        self.location = QtGui.QComboBox(self.groupBox)
+        self.location.setEditable(True)
+        self.location.setObjectName("location")
+        self.gridlayout.addWidget(self.location,0,1,1,2)
+
+        self.label_3 = QtGui.QLabel(self.groupBox)
+        self.label_3.setObjectName("label_3")
+        self.gridlayout.addWidget(self.label_3,1,0,1,1)
+
+        self.revision = QtGui.QLineEdit(self.groupBox)
+        self.revision.setObjectName("revision")
+        self.gridlayout.addWidget(self.revision,1,1,1,1)
+
+        spacerItem = QtGui.QSpacerItem(211,20,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
+        self.gridlayout.addItem(spacerItem,1,2,1,1)
+
+        self.remember = QtGui.QCheckBox(self.groupBox)
+        self.remember.setChecked(True)
+        self.remember.setObjectName("remember")
+        self.gridlayout.addWidget(self.remember,2,0,1,3)
+
+        self.overwrite = QtGui.QCheckBox(self.groupBox)
+        self.overwrite.setObjectName("overwrite")
+        self.gridlayout.addWidget(self.overwrite,3,0,1,3)
+        self.vboxlayout.addWidget(self.groupBox)
+
+        self.groupBox_2 = QtGui.QGroupBox(PullForm)
+        self.groupBox_2.setObjectName("groupBox_2")
+
+        self.vboxlayout1 = QtGui.QVBoxLayout(self.groupBox_2)
+        self.vboxlayout1.setObjectName("vboxlayout1")
+
+        self.progressMessage = QtGui.QLabel(self.groupBox_2)
+        self.progressMessage.setWordWrap(True)
+        self.progressMessage.setObjectName("progressMessage")
+        self.vboxlayout1.addWidget(self.progressMessage)
+
+        self.progressBar = QtGui.QProgressBar(self.groupBox_2)
+        self.progressBar.setMaximum(1000000)
+        self.progressBar.setObjectName("progressBar")
+        self.vboxlayout1.addWidget(self.progressBar)
+
+        self.console = QtGui.QTextBrowser(self.groupBox_2)
+        self.console.setObjectName("console")
+        self.vboxlayout1.addWidget(self.console)
+        self.vboxlayout.addWidget(self.groupBox_2)
+        self.label_2.setBuddy(self.location)
+        self.label_3.setBuddy(self.revision)
+
+        self.retranslateUi(PullForm)
+        QtCore.QMetaObject.connectSlotsByName(PullForm)
+
+    def retranslateUi(self, PullForm):
+        self.groupBox.setTitle(gettext("Options"))
+        self.label_2.setText(gettext("&Location:"))
+        self.label_3.setText(gettext("&Revision:"))
+        self.remember.setText(gettext("Remember this location as a default"))
+        self.overwrite.setText(gettext("Overwrite differences between branches"))
+        self.groupBox_2.setTitle(gettext("Status"))
+        self.progressMessage.setText(gettext("Stopped"))
+
---
+++ ui_push.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui/push.ui'
+#
+# Created: Thu Dec 13 18:42:13 2007
+#      by: PyQt4 UI code generator 4.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+from bzrlib.plugins.qbzr.i18n import gettext
+
+class Ui_PushForm(object):
+    def setupUi(self, PushForm):
+        PushForm.setObjectName("PushForm")
+        PushForm.resize(QtCore.QSize(QtCore.QRect(0,0,429,400).size()).expandedTo(PushForm.minimumSizeHint()))
+
+        self.vboxlayout = QtGui.QVBoxLayout(PushForm)
+        self.vboxlayout.setObjectName("vboxlayout")
+
+        self.groupBox = QtGui.QGroupBox(PushForm)
+        self.groupBox.setObjectName("groupBox")
+
+        self.gridlayout = QtGui.QGridLayout(self.groupBox)
+        self.gridlayout.setObjectName("gridlayout")
+
+        self.label_2 = QtGui.QLabel(self.groupBox)
+        self.label_2.setObjectName("label_2")
+        self.gridlayout.addWidget(self.label_2,0,0,1,1)
+
+        self.location = QtGui.QComboBox(self.groupBox)
+
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.location.sizePolicy().hasHeightForWidth())
+        self.location.setSizePolicy(sizePolicy)
+        self.location.setEditable(True)
+        self.location.setObjectName("location")
+        self.gridlayout.addWidget(self.location,0,1,1,1)
+
+        self.remember = QtGui.QCheckBox(self.groupBox)
+        self.remember.setChecked(True)
+        self.remember.setObjectName("remember")
+        self.gridlayout.addWidget(self.remember,1,0,1,2)
+
+        self.overwrite = QtGui.QCheckBox(self.groupBox)
+        self.overwrite.setObjectName("overwrite")
+        self.gridlayout.addWidget(self.overwrite,2,0,1,2)
+
+        self.use_existing_dir = QtGui.QCheckBox(self.groupBox)
+        self.use_existing_dir.setObjectName("use_existing_dir")
+        self.gridlayout.addWidget(self.use_existing_dir,3,0,1,2)
+
+        self.create_prefix = QtGui.QCheckBox(self.groupBox)
+        self.create_prefix.setObjectName("create_prefix")
+        self.gridlayout.addWidget(self.create_prefix,4,0,1,2)
+        self.vboxlayout.addWidget(self.groupBox)
+
+        self.groupBox_2 = QtGui.QGroupBox(PushForm)
+        self.groupBox_2.setObjectName("groupBox_2")
+
+        self.vboxlayout1 = QtGui.QVBoxLayout(self.groupBox_2)
+        self.vboxlayout1.setObjectName("vboxlayout1")
+
+        self.progressMessage = QtGui.QLabel(self.groupBox_2)
+        self.progressMessage.setWordWrap(True)
+        self.progressMessage.setObjectName("progressMessage")
+        self.vboxlayout1.addWidget(self.progressMessage)
+
+        self.progressBar = QtGui.QProgressBar(self.groupBox_2)
+        self.progressBar.setMaximum(1000000)
+        self.progressBar.setObjectName("progressBar")
+        self.vboxlayout1.addWidget(self.progressBar)
+
+        self.console = QtGui.QTextBrowser(self.groupBox_2)
+        self.console.setObjectName("console")
+        self.vboxlayout1.addWidget(self.console)
+        self.vboxlayout.addWidget(self.groupBox_2)
+        self.label_2.setBuddy(self.location)
+
+        self.retranslateUi(PushForm)
+        QtCore.QMetaObject.connectSlotsByName(PushForm)
+
+    def retranslateUi(self, PushForm):
+        self.groupBox.setTitle(gettext("Options"))
+        self.label_2.setText(gettext("&Location:"))
+        self.remember.setText(gettext("Remember this location as a default"))
+        self.overwrite.setText(gettext("Overwrite differences between branches"))
+        self.use_existing_dir.setText(gettext("Use existing directory"))
+        self.create_prefix.setText(gettext("Create the path up to the branch if it does not exist"))
+        self.groupBox_2.setTitle(gettext("Status"))
+        self.progressMessage.setText(gettext("Stopped"))
+
