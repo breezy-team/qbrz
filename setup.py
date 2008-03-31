e03c7ebe8a9ed6a50359ae49c43ddb5a480da078
Alexander Belchenko <bialix@ukr.net>
2008-03-31 17:42:15 +0300

    starting 0.9.1

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ if have_pyqt_dev:
 setup(name='qbzr',
       description='Qt4 frontend for Bazaar',
       keywords='plugin bzr qt qbzr',
-      version='0.9.0',
+      version='0.9.1',
       url='http://bazaar-vcs.org/QBzr',
       license='GPL',
       author='Lukáš Lalinský',
