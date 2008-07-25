abe44b6ff8584b140f3ba24eaf3cff5248b436be
Lukáš Lalinský <lalinsky@gmail.com>
2008-07-25 09:05:13 +0200

    Fix copyright

--- lib/test_diffview.py
+++ lib/test_diffview.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2008 Alexander Belchenko
+# Copyright (C) 2008 Lukáš Lalinský <lalinsky@gmail.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
