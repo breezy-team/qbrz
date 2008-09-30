70979353fa57e1ddc76d5e883968d9571e211d0b
Alexander Belchenko <bialix@ukr.net>
2008-09-30 23:58:29 +0300

    show actual revno/revid for selected tag when delete action selected.

--- lib/tag.py
+++ lib/tag.py
@@ -39,32 +39,58 @@ class TagWindow(SubProcessDialog):
 
         self.ui = Ui_TagForm()
         self.ui.setupUi(self)
+        self.last_action = self.ui.cb_action.currentIndex()
 
         # and add the subprocess widgets.
         for w in self.make_default_layout_widgets():
             self.layout().addWidget(w)
         self.process_widget.hide_progress()
 
+        self.set_branch(branch)
+
         # setup signals
         QtCore.QObject.connect(self.ui.cb_action,
             QtCore.SIGNAL("currentIndexChanged(int)"),
-            self.action_changed)
+            self.on_action_changed)
+        QtCore.QObject.connect(self.ui.cb_tag,
+            QtCore.SIGNAL("currentIndexChanged(int)"),
+            self.on_tag_changed)
         # groupbox gets disabled as we are executing.
         QtCore.QObject.connect(self,
                                QtCore.SIGNAL("subprocessStarted(bool)"),
                                self.ui.tag_group,
                                QtCore.SLOT("setDisabled(bool)"))
 
-        self.set_branch(branch)
-
     def set_branch(self, branch):
         self.branch = branch
-        self.tags = sorted(branch.tags.get_tag_dict().keys())
+        self.tags = branch.tags.get_tag_dict()
+        self.revno_map = None
+        self.rev_text = {}
         # update ui
         self.ui.branch_location.setText(url_for_display(branch.base))
         self.ui.cb_tag.clear()
-        self.ui.cb_tag.addItems(QtCore.QStringList(self.tags))
+        self.ui.cb_tag.addItems(QtCore.QStringList(sorted(self.tags.keys())))
         self.ui.cb_tag.setEditText("")
 
-    def action_changed(self, index):
-        self.ui.cb_tag.setEditText("")
+    def on_action_changed(self, index):
+        #self.ui.cb_tag.setEditText("")
+        self.rev_text[self.last_action] = self.ui.rev_edit.text()
+        self.ui.rev_edit.setDisabled(index == self.IX_DELETE)
+        self.ui.rev_edit.setText(self.rev_text.get(index, ''))
+        self.last_action = index
+        self.on_tag_changed()
+
+    def on_tag_changed(self, index=None):
+        if self.ui.cb_action.currentIndex() == self.IX_DELETE:
+            tag = unicode(self.ui.cb_tag.currentText())
+            revid = self.tags.get(tag)
+            if revid:
+                # get revno
+                if self.revno_map is None:
+                    self.revno_map = self.branch.get_revision_id_to_revno_map()
+                rt = self.revno_map.get(revid)
+                if rt:
+                    rev_str = '.'.join(map(str, rt))
+                else:
+                    rev_str = 'revid:'+revid
+                self.ui.rev_edit.setText(rev_str)
