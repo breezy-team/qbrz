34ad65ea207269291f3c32b861bcdfc73127a89f
Alexander Belchenko <bialix@ukr.net>
2008-10-01 20:52:36 +0300

    changing layout

--- lib/ui_tag.py
+++ lib/ui_tag.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/tag.ui'
 #
-# Created: Wed Oct 01 11:04:31 2008
+# Created: Wed Oct 01 20:31:21 2008
 #      by: PyQt4 UI code generator 4.3.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -15,7 +15,7 @@ class Ui_TagForm(object):
     def setupUi(self, TagForm):
         TagForm.setObjectName("TagForm")
         TagForm.setWindowModality(QtCore.Qt.NonModal)
-        TagForm.resize(QtCore.QSize(QtCore.QRect(0,0,400,164).size()).expandedTo(TagForm.minimumSizeHint()))
+        TagForm.resize(QtCore.QSize(QtCore.QRect(0,0,391,220).size()).expandedTo(TagForm.minimumSizeHint()))
 
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred,QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
@@ -28,58 +28,64 @@ class Ui_TagForm(object):
         self.vboxlayout = QtGui.QVBoxLayout(TagForm)
         self.vboxlayout.setObjectName("vboxlayout")
 
-        self.tag_group = QtGui.QGroupBox(TagForm)
-        self.tag_group.setMinimumSize(QtCore.QSize(373,146))
-        self.tag_group.setObjectName("tag_group")
+        self.branch_group = QtGui.QGroupBox(TagForm)
+        self.branch_group.setObjectName("branch_group")
 
-        self.gridLayout = QtGui.QWidget(self.tag_group)
-        self.gridLayout.setGeometry(QtCore.QRect(10,10,361,131))
-        self.gridLayout.setObjectName("gridLayout")
-
-        self.gridlayout = QtGui.QGridLayout(self.gridLayout)
+        self.gridlayout = QtGui.QGridLayout(self.branch_group)
         self.gridlayout.setObjectName("gridlayout")
 
-        self.label_branch = QtGui.QLabel(self.gridLayout)
+        self.label_branch = QtGui.QLabel(self.branch_group)
         self.label_branch.setObjectName("label_branch")
         self.gridlayout.addWidget(self.label_branch,0,0,1,1)
 
-        self.branch_location = QtGui.QLineEdit(self.gridLayout)
+        self.branch_location = QtGui.QLineEdit(self.branch_group)
         self.branch_location.setObjectName("branch_location")
-        self.gridlayout.addWidget(self.branch_location,0,1,1,1)
+        self.gridlayout.addWidget(self.branch_location,0,1,1,2)
 
-        self.branch_browse = QtGui.QPushButton(self.gridLayout)
+        spacerItem = QtGui.QSpacerItem(40,20,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
+        self.gridlayout.addItem(spacerItem,1,1,1,1)
+
+        self.branch_browse = QtGui.QPushButton(self.branch_group)
         self.branch_browse.setObjectName("branch_browse")
-        self.gridlayout.addWidget(self.branch_browse,0,2,1,1)
+        self.gridlayout.addWidget(self.branch_browse,1,2,1,1)
+        self.vboxlayout.addWidget(self.branch_group)
 
-        self.label_action = QtGui.QLabel(self.gridLayout)
+        self.tag_group = QtGui.QGroupBox(TagForm)
+        self.tag_group.setMinimumSize(QtCore.QSize(0,0))
+        self.tag_group.setObjectName("tag_group")
+
+        self.gridlayout1 = QtGui.QGridLayout(self.tag_group)
+        self.gridlayout1.setObjectName("gridlayout1")
+
+        self.label_action = QtGui.QLabel(self.tag_group)
         self.label_action.setObjectName("label_action")
-        self.gridlayout.addWidget(self.label_action,1,0,1,1)
+        self.gridlayout1.addWidget(self.label_action,0,0,1,1)
 
-        self.cb_action = QtGui.QComboBox(self.gridLayout)
+        self.cb_action = QtGui.QComboBox(self.tag_group)
         self.cb_action.setObjectName("cb_action")
-        self.gridlayout.addWidget(self.cb_action,1,1,1,1)
+        self.gridlayout1.addWidget(self.cb_action,0,1,1,1)
 
-        self.label_tag_name = QtGui.QLabel(self.gridLayout)
+        self.label_tag_name = QtGui.QLabel(self.tag_group)
         self.label_tag_name.setObjectName("label_tag_name")
-        self.gridlayout.addWidget(self.label_tag_name,2,0,1,1)
+        self.gridlayout1.addWidget(self.label_tag_name,1,0,1,1)
 
-        self.cb_tag = QtGui.QComboBox(self.gridLayout)
+        self.cb_tag = QtGui.QComboBox(self.tag_group)
         self.cb_tag.setEditable(True)
         self.cb_tag.setObjectName("cb_tag")
-        self.gridlayout.addWidget(self.cb_tag,2,1,1,1)
+        self.gridlayout1.addWidget(self.cb_tag,1,1,1,1)
 
-        self.label_revision = QtGui.QLabel(self.gridLayout)
+        self.label_revision = QtGui.QLabel(self.tag_group)
         self.label_revision.setObjectName("label_revision")
-        self.gridlayout.addWidget(self.label_revision,3,0,1,1)
+        self.gridlayout1.addWidget(self.label_revision,2,0,1,1)
 
-        self.rev_edit = QtGui.QLineEdit(self.gridLayout)
+        self.rev_edit = QtGui.QLineEdit(self.tag_group)
         self.rev_edit.setObjectName("rev_edit")
-        self.gridlayout.addWidget(self.rev_edit,3,1,1,1)
+        self.gridlayout1.addWidget(self.rev_edit,2,1,1,1)
 
-        self.pick_rev = QtGui.QPushButton(self.gridLayout)
+        self.pick_rev = QtGui.QPushButton(self.tag_group)
         self.pick_rev.setEnabled(False)
         self.pick_rev.setObjectName("pick_rev")
-        self.gridlayout.addWidget(self.pick_rev,3,2,1,1)
+        self.gridlayout1.addWidget(self.pick_rev,2,2,1,1)
         self.vboxlayout.addWidget(self.tag_group)
         self.label_branch.setBuddy(self.branch_location)
         self.label_action.setBuddy(self.cb_action)
@@ -95,10 +101,11 @@ class Ui_TagForm(object):
         TagForm.setTabOrder(self.rev_edit,self.pick_rev)
 
     def retranslateUi(self, TagForm):
-        TagForm.setWindowTitle(gettext("Tag"))
-        self.tag_group.setTitle(gettext("Edit tag"))
+        TagForm.setWindowTitle(gettext("Edit tag"))
+        self.branch_group.setTitle(gettext("Branch"))
         self.label_branch.setText(gettext("&Branch:"))
         self.branch_browse.setText(gettext("Browse..."))
+        self.tag_group.setTitle(gettext("Tag"))
         self.label_action.setText(gettext("&Action:"))
         self.cb_action.addItem(gettext("Create new tag"))
         self.cb_action.addItem(gettext("Replace existing tag"))
