b1fc7c9538df49dd42ae6659fa453888b6bd3224
Gary van der Merwe <garyvdm@gmail.com>
2009-01-20 19:48:29 +0200

    Implement QUIFactory.clear_term.

--- lib/uifactory.py
+++ lib/uifactory.py
@@ -101,3 +101,12 @@ class QUIFactory(ui.UIFactory):
             return str(password)
         else:
             raise KeyboardInterrupt()
+
+    def clear_term(self):
+        """Prepare the terminal for output.
+
+        This will, for example, clear text progress bars, and leave the
+        cursor at the leftmost position."""
+        pass
+    
+    # TODO: Implement get_boolean
