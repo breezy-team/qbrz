f9dcf56e8c08ffcdc43fd5b3526256693eabd3b0
Alexander Belchenko <bialix@ukr.net>
2009-02-02 04:57:26 +0200

    python 2.4 compatibility

--- lib/util.py
+++ lib/util.py
@@ -859,7 +859,7 @@ def is_binary_content(lines):
             return True
     return False
 
-class BackgroundJob():
+class BackgroundJob(object):
     
     def __init__(self, parent):
         self.is_running = False
