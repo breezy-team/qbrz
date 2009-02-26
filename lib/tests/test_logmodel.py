32c7b4933b0aadd954bdbc01a58628ebd58e210e
Craig Hewetson <craighewetson@gmail.com>
2009-02-26 11:46:07 +0200

    added test case for fogbugz url

--- lib/tests/test_logmodel.py
+++ lib/tests/test_logmodel.py
@@ -35,6 +35,9 @@ class TestGetBugId(TestCase):
     def test_redmine(self):
         self.assertEquals('1832', get_bug_id('http://www.redmine.org/issues/show/1832'))
 
+    def test_fogbugz(self):
+        self.assertEquals('1234', get_bug_id('http://test.fogbugz.com/default.asp?1234'))
+
 
 class TestQVariantFromList(TestCase):
 
