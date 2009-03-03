16f51eca67d9524844563979428d6ff606713931
Craig Hewetson <craighewetson@gmail.com>
2009-03-03 09:34:20 +0200

    Added a feature that allows the user to make use of the same convention that the extmerge plugin uses to execute an installed merge tool and pass it the BASE THIS and OTHER files in the correct order. The change included an additional check box on the ui. To use this feature the user must insert an external_merge key in the .bazaar.conf file with a value currently specified by the extmerge plugin. Example: external_merge = p4v -merge %b %o %t %r

--- lib/conflicts.py
+++ lib/conflicts.py
@@ -18,11 +18,13 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 import sys
+import exceptions
 from PyQt4 import QtCore, QtGui
 from bzrlib import (
     osutils,
     errors,
     )
+from bzrlib.config import GlobalConfig
 from bzrlib.conflicts import resolve
 from bzrlib.workingtree import WorkingTree
 from bzrlib.plugins.qbzr.lib.i18n import gettext, N_, ngettext
@@ -74,12 +76,20 @@ class ConflictsWindow(QBzrWindow):
         hbox = QtGui.QHBoxLayout()
         self.program_edit = QtGui.QLineEdit(self)
         self.program_edit.setEnabled(False)
-        config = QBzrGlobalConfig()
-        self.program_edit.setText((config.get_user_option("merge_tool") or "").strip() or "meld")
         self.connect(
             self.program_edit,
             QtCore.SIGNAL("textChanged(QString)"),
             self.check_merge_tool_edit)
+        self.program_extmerge_default_button = QtGui.QCheckBox(gettext("Use Configured Default"))
+        self.program_extmerge_default_button.setToolTip(gettext(
+                "The merge tool configured in your '.bazaar.conf' file.\n"
+                "It follows the convention used in the bzr plugin: extmerge\n"
+                "external_merge = kdiff3 --output %r %b %t %o\n"
+                "%r is output, %b is .BASE, %t is .THIS and %o is .OTHER file."))
+        self.connect(
+            self.program_extmerge_default_button,
+            QtCore.SIGNAL("clicked()"),
+            self.program_extmerge_default_clicked)
         self.program_launch_button = QtGui.QPushButton(gettext("&Launch..."), self)
         self.program_launch_button.setEnabled(False)
         self.connect(
@@ -90,6 +100,7 @@ class ConflictsWindow(QBzrWindow):
         self.program_label.setBuddy(self.program_edit)
         hbox.addWidget(self.program_label)
         hbox.addWidget(self.program_edit)
+        hbox.addWidget(self.program_extmerge_default_button)
         hbox.addWidget(self.program_launch_button)
         vbox.addLayout(hbox)
 
@@ -109,6 +120,24 @@ class ConflictsWindow(QBzrWindow):
         hbox.addWidget(autobutton)
         hbox.addWidget(buttonbox)
         vbox.addLayout(hbox)
+        self.initialize_ui()        
+
+    def initialize_ui(self):
+        config = QBzrGlobalConfig()
+        merge_tool_extmerge = config.get_user_option("merge_tool_extmerge")
+        self.program_extmerge_default_button.setCheckState(QtCore.Qt.Unchecked)
+        if merge_tool_extmerge in ("True", "1"):
+          self.program_extmerge_default_button.setCheckState(QtCore.Qt.Checked)
+        self.program_extmerge_default_clicked()
+        enabled, error_msg = self.is_merge_tool_launchable()
+        self.update_program_edit_text(enabled, error_msg)
+        # if extmerge not configured then resort to using default
+        if not enabled and self.program_extmerge_default_button.isChecked():
+          bzr_config = GlobalConfig()
+          extmerge_tool = bzr_config.get_user_option("external_merge")
+          if not extmerge_tool:
+            self.program_extmerge_default_button.setCheckState(QtCore.Qt.Unchecked)
+            self.update_program_edit_text(False, "")
 
     def show(self):
         QBzrWindow.show(self)
@@ -149,36 +178,54 @@ class ConflictsWindow(QBzrWindow):
             self.close()
 
     def update_selection(self, selected, deselected):
-        items = self.conflicts_list.selectedItems()
-        enabled = True
-        if len(items) != 1 or items[0].data(1, QtCore.Qt.UserRole).toString() != "text conflict":
-            enabled = False
-        self.program_edit.setEnabled(enabled)
-        if enabled:
-            self.check_merge_tool_edit(self.program_edit.text())
-        else:
-            self.program_launch_button.setEnabled(enabled)
+        enabled, error_msg = self.is_merge_tool_launchable()
+        self.program_edit.setEnabled(enabled and not self.program_extmerge_default_button.isChecked())
+        self.program_launch_button.setEnabled(enabled)
+        self.update_program_edit_text(enabled, error_msg)
+        if enabled and self.program_extmerge_default_button.isChecked():
+          self.program_edit.setEnabled(False)
 
     def check_merge_tool_edit(self, text):
-        self.program_launch_button.setEnabled(not text.isEmpty())
+        enabled, error_msg = self.is_merge_tool_launchable()
+        self.program_launch_button.setEnabled(enabled)
 
     def launch_merge_tool(self):
         items = self.conflicts_list.selectedItems()
-        if len(items) != 1 or items[0].data(1, QtCore.Qt.UserRole).toString() != "text conflict":
-            return
+        enabled, error_msg = self.is_merge_tool_launchable()
+        if not enabled:
+          return
         merge_tool = unicode(self.program_edit.text()).strip()
         if not merge_tool:
-            return
+          return
         file_id = str(items[0].data(0, QtCore.Qt.UserRole).toString())
         file_name = self.wt.abspath(self.wt.id2path(file_id))
         base_file_name = file_name + ".BASE"
         this_file_name = file_name + ".THIS"
         other_file_name = file_name + ".OTHER"
+        new_args = [base_file_name, this_file_name, other_file_name]
         config = QBzrGlobalConfig()
-        config.set_user_option("merge_tool", merge_tool)
+        config.set_user_option("merge_tool_extmerge", False)
+
+        if self.program_extmerge_default_button.isChecked():
+          bzr_config = GlobalConfig()
+          extmerge_tool = bzr_config.get_user_option("external_merge")
+          args = extmerge_tool.split(" ")
+          new_args = args[1:len(args)]
+          i = 0
+          while i < len(new_args):
+            new_args[i] = new_args[i].replace('%r', file_name)
+            new_args[i] = new_args[i].replace('%o', other_file_name)
+            new_args[i] = new_args[i].replace('%b', base_file_name)
+            new_args[i] = new_args[i].replace('%t', this_file_name)
+            i = i + 1
+          merge_tool = args[0]
+          config.set_user_option("merge_tool_extmerge", True)
+        else:
+          config.set_user_option("merge_tool", merge_tool)
+
         process = QtCore.QProcess(self)
         self.connect(process, QtCore.SIGNAL("error(QProcess::ProcessError)"), self.show_merge_tool_error)
-        process.start(merge_tool, [base_file_name, this_file_name, other_file_name])
+        process.start(merge_tool, new_args)
 
     def show_merge_tool_error(self, error):
         msg = gettext("Error while running merge tool (code %d)") % error
@@ -196,6 +243,72 @@ class ConflictsWindow(QBzrWindow):
     def show_context_menu(self, pos):
         self.context_menu.popup(self.conflicts_list.viewport().mapToGlobal(pos))
 
+    def program_extmerge_default_clicked(self):	
+        enabled, error_msg = self.is_merge_tool_launchable()
+        self.program_edit.setEnabled(enabled and not self.program_extmerge_default_button.isChecked())
+        self.program_launch_button.setEnabled(enabled)
+        self.update_program_edit_text(enabled, error_msg)
+        config = QBzrGlobalConfig()
+        config.set_user_option("merge_tool_extmerge", False)
+        if self.program_extmerge_default_button.isChecked():
+          config.set_user_option("merge_tool_extmerge", True)  
+
+    def is_merge_tool_launchable(self):
+        items = self.conflicts_list.selectedItems()
+	error_msg = ""
+        enabled = True
+        if len(items) != 1 or items[0].data(1, QtCore.Qt.UserRole).toString() != "text conflict":
+          enabled = False
+
+        # check to see if the extmerge config is correct   
+        if self.program_extmerge_default_button.isChecked():
+          bzr_config = GlobalConfig()
+          extmerge_tool = bzr_config.get_user_option("external_merge")
+          if not extmerge_tool:
+            error_msg = gettext("Set up external_merge value in .bazaar.conf")
+            enabled = False
+            return enabled, error_msg
+            
+          error = self.is_extmerge_definition_valid(False)
+          if len(error) > 0:
+            enabled = False
+            error_msg = error
+
+        return enabled, error_msg
+
+    def is_extmerge_definition_valid(self, showErrorDialog):
+        bzr_config = GlobalConfig()
+        extmerge_tool = bzr_config.get_user_option("external_merge")
+        # Check if the definition format is correct
+        flags = "%r"
+        try:
+          extmerge_tool.rindex('%r')
+	  flags = "%b"
+	  extmerge_tool.rindex('%b')
+	  flags = "%t"
+	  extmerge_tool.rindex('%t')
+	  flags = "%o"
+	  extmerge_tool.rindex('%o')
+        except exceptions.ValueError, e:
+          if showErrorDialog:
+            QtGui.QMessageBox.critical(self, gettext("Error"),
+            gettext("The extmerge tool definition in .bazaar.conf file:\n'"
+                    +extmerge_tool + "' is incomplete.\nMissing the flag: "+flags))
+          return gettext("Missing the flag: "+flags+". For extmerge config definition in .bazaar.conf.")
+        return ""
+
+    def update_program_edit_text(self, enabled, error_msg):
+        if self.program_extmerge_default_button.isChecked():
+          if enabled or (len(error_msg) <= 0):
+            config = GlobalConfig()
+            extmerge = config.get_user_option("external_merge")
+            self.program_edit.setText(gettext(extmerge+" (external_merge value in .bazaar.conf)"))
+          else:
+            self.program_edit.setText(error_msg)
+        else:
+          config = QBzrGlobalConfig()
+          self.program_edit.setText((config.get_user_option("merge_tool") or "").strip() or "meld")
+          
 
 if 0:
     N_("path conflict")
