92cdfab5f1ed8cb0369cc9ef2ae8ca1ce1b411ba
Gary van der Merwe <garyvdm@gmail.com>
2009-06-22 19:52:46 +0200

    Small change to get unversioned directories working.

--- lib/treewidget.py
+++ lib/treewidget.py
@@ -113,15 +113,6 @@ class TreeModel(QtCore.QAbstractItemModel):
             yield (child, None)
     
     def working_tree_get_children(self, item):
-    #[0]: file_id         -> ascii string
-    #[1]: paths           -> 2-tuple (old, new) fullpaths unicode/None
-    #[2]: changed_content -> bool
-    #[3]: versioned       -> 2-tuple (bool, bool)
-    #[4]: parent          -> 2-tuple
-    #[5]: name            -> 2-tuple (old_name, new_name) utf-8?/None
-    #[6]: kind            -> 2-tuple (string/None, string/None)
-    #[7]: executable      -> 2-tuple (bool/None, bool/None)
-
         if isinstance(item, UnversionedItem):
             abspath = self.tree.abspath(item.path)
             
@@ -129,7 +120,7 @@ class TreeModel(QtCore.QAbstractItemModel):
                 path = item.path+"/"+name
                 (kind,
                  executable,
-                 stat_value) = tree._comparison_data(None, path)
+                 stat_value) = self.tree._comparison_data(None, path)
                 item = UnversionedItem(name, path, kind)
                 is_ignored = self.tree.is_ignored(path)
                 change = ChangeDesc((None,
