4b4700276722579a547d22b9910159c710c13b12
Javier Derderian <javierder@gmail.com>
2009-08-13 11:22:33 -0300

    Fixed bug #412058. Throbber doesn't block ui anymore

--- lib/switch.py
+++ lib/switch.py
@@ -55,14 +55,19 @@ class QBzrSwitchWindow(SubProcessDialog):
         
         gbSwitch = QtGui.QGroupBox(gettext("Switch Checkout to"), self)
         
-        switch_hbox = QtGui.QHBoxLayout(gbSwitch)
+        switch_vbox = QtGui.QVBoxLayout(gbSwitch)
+        
+        throb_hbox = QtGui.QHBoxLayout()
         
         self.throbber = ThrobberWidget(self)
+        throb_hbox.addWidget(self.throbber)
+        switch_vbox.addLayout(throb_hbox)
+        
+        switch_hbox = QtGui.QHBoxLayout()
         
         branch_label = QtGui.QLabel(gettext("Branch:"))
         branch_combo = QtGui.QComboBox()   
         branch_combo.setEditable(True)
-        branch_combo.hide()
         
         self.branch_combo = branch_combo
         
@@ -75,20 +80,18 @@ class QBzrSwitchWindow(SubProcessDialog):
         self.boundloc = boundloc
         if boundloc != None:
             branch_combo.addItem(url_for_display(boundloc))
-
-
         browse_button = QtGui.QPushButton(gettext("Browse"))
         QtCore.QObject.connect(browse_button, QtCore.SIGNAL("clicked(bool)"), self.browse_clicked)
         
-                
         switch_hbox.addWidget(branch_label)
         switch_hbox.addWidget(branch_combo)
-        switch_hbox.addWidget(self.throbber)
         switch_hbox.addWidget(browse_button)
         
         switch_hbox.setStretchFactor(branch_label,0)
         switch_hbox.setStretchFactor(branch_combo,1)
         switch_hbox.setStretchFactor(browse_button,0)
+        
+        switch_vbox.addLayout(switch_hbox)
         
         layout = QtGui.QVBoxLayout(self)
         
@@ -103,7 +106,7 @@ class QBzrSwitchWindow(SubProcessDialog):
 
     @runs_in_loading_queue
     @ui_current_widget
-    @reports_exception()
+    @reports_exception(type=SUB_LOAD_METHOD)   
     def initial_load(self):
         
         self.throbber.show()
@@ -117,7 +120,6 @@ class QBzrSwitchWindow(SubProcessDialog):
                 branch_combo.addItem(url_for_display(br.base))
                 
         self.throbber.hide()
-        self.branch_combo.show()
              
         if self.boundloc == None:
             branch_combo.clearEditText()
