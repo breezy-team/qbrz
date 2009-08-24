c3713c8a0f8fd2bbaa1eb697398f38980cb04e58
Alexander Belchenko <bialix@ukr.net>
2009-08-24 22:13:48 +0300

    Updated build release (on Windows) process.
    1) make release: updated for new universal windows installer way; 
    2) added helper script copy_libs.py to copy 3rd party libs to installer/_lib; 
    3) updated instructions make_release.txt;
    4) completely removed old nsis support.

--- extras/__init__.py
+++ extras/__init__.py
@@ -18,7 +18,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-from bdist_nsis import bdist_nsis
 from build_docs import build_docs
 from build_mo import build_mo
 from build_pot import build_pot
@@ -29,7 +28,6 @@ from import_po import import_po
 
 
 cmdclass = {
-    'bdist_nsis': bdist_nsis,
     'build_docs': build_docs,
     'build_mo': build_mo,
     'build_pot': build_pot,
