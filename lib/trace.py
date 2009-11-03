31b782dc7281de917409c67c45c0c6423ad95adb
Gary van der Merwe <garyvdm@gmail.com>
2009-11-03 19:24:02 +0200

    Add docs on how to use the exception reporting.

--- lib/trace.py
+++ lib/trace.py
@@ -18,6 +18,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
+"""Exception handeling and reporting.
+
+Please see docs/exception_reporting.txt for info on how to use this.
+"""
+
 import sys
 import os
 
