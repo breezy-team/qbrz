f2c4f9664921a069289417acab5d993033bcff38
Gary van der Merwe <garyvdm@gmail.com>
2009-11-06 14:05:38 +0200

    qlog: Fix a bug when refreshing a branch with revisions that have been removed.

--- lib/loggraphprovider.py
+++ lib/loggraphprovider.py
@@ -854,6 +854,8 @@ class LogGraphProvider(object):
     def get_revision_visible(self, index):
         """ Returns wether a revision is visible or not"""
         
+        if not index < len(self.revisions):
+            return False
         
         return self.revisions[index].f_index is not None
         #branch_id = self.revisions[index].branch_id
