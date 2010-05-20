29a54f424f2374b79bcc9145647caa45826baac8
Gary van der Merwe <garyvdm@gmail.com>
2010-05-20 12:41:15 +0200

    qannotate: Stable scroll and selection.

--- lib/annotate.py
+++ lib/annotate.py
@@ -362,15 +362,17 @@ class AnnotateWindow(QBzrWindow):
                 self.processEvents()
         annotate.append((None, False))  # because the view has one more line
         
+        new_positions = None
         if self.old_lines:
             # Try keep the scroll, and selection stable.
             sm = SequenceMatcher(None, self.old_lines, lines)
             opcodes = sm.get_opcodes()
             old_cursor = self.text_edit.textCursor()
-            old_center_y = (self.text_edit.verticalScrollBar().value() +
-                            self.text_edit.height() / 2)
+            
             old_center = self.text_edit.cursorForPosition(
-                QtCore.QPoint(0, old_center_y))
+                QtCore.QPoint(0, self.text_edit.height() / 2))
+            lines_to_center = (old_center.block().blockNumber() -
+                               self.text_edit.verticalScrollBar().value())
             
             old_positions = (
                 old_cursor.selectionStart(),
@@ -388,15 +390,32 @@ class AnnotateWindow(QBzrWindow):
                     new_len = opcode_len(new_start, new_end, lines)
                     if (old_pos >= old_char_start and
                         old_pos < old_char_start + old_len):
-                        new_pos = new_char_start + (old_pos - old_char_start)
+                        if code == 'delete':
+                            new_pos = new_char_start
+                        else:
+                            new_pos = new_char_start + (old_pos - old_char_start)
                         new_positions[i] = new_pos
                         break
                     old_char_start += old_len
                     new_char_start += new_len
-            print old_positions
-            print new_positions
         
         self.text_edit.setPlainText("".join(lines))
+        if new_positions:
+            new_start, new_end, new_center = new_positions
+            new_center_cursor = QtGui.QTextCursor(self.text_edit.document())
+            new_center_cursor.setPosition(new_center)
+            new_scroll = new_center_cursor.block().blockNumber() - lines_to_center
+            self.text_edit.verticalScrollBar().setValue(new_scroll)
+            
+            new_selection_cursor = QtGui.QTextCursor(self.text_edit.document())
+            new_selection_cursor.movePosition(QtGui.QTextCursor.Right,
+                                              QtGui.QTextCursor.MoveAnchor,
+                                              new_start)
+            new_selection_cursor.movePosition(QtGui.QTextCursor.Right,
+                                              QtGui.QTextCursor.KeepAnchor,
+                                              new_end - new_start)
+            self.text_edit.setTextCursor(new_selection_cursor)
+        
         self.old_lines = lines
         self.annotate_bar.adjustWidth(len(lines), 999)
         self.annotate_bar.annotate = annotate
