405c0db218184588c8072f0fab4f66b4675b0d2d
Gary van der Merwe <garyvdm@gmail.com>
2010-08-31 19:12:31 +0200

    test_basic_branch_line

--- lib/tests/test_loggraphprovider.py
+++ lib/tests/test_loggraphprovider.py
@@ -18,14 +18,23 @@
 
 from bzrlib.tests import TestCase, TestCaseWithTransport
 
+
 from bzrlib.plugins.qbzr.lib import loggraphprovider
 
 class TestLogGraphProvider(TestCaseWithTransport):
     
+    def assertComputed(self, expected_list, computed):
+        computed_list = [(c_rev.rev.revid,
+                          c_rev.col_index,
+                          c_rev.twisty_state,
+                          c_rev.lines,)
+                         for c_rev in computed.filtered_revs]
+        self.assertEqual(expected_list, computed_list)
+    
     def test_no_commits(self):
-        wt = self.make_branch_and_tree('.')
+        br = self.make_branch('.')
         
-        bi = loggraphprovider.BranchInfo('', wt, wt.branch)
+        bi = loggraphprovider.BranchInfo('', None, br)
         gp = loggraphprovider.LogGraphProvider([bi], bi, False)
         gp.load()
         
@@ -34,3 +43,39 @@ class TestLogGraphProvider(TestCaseWithTransport):
         state = loggraphprovider.GraphProviderFilterState(gp)
         computed = gp.compute_graph_lines(state)
         self.assertEqual(len(computed.revisions), 0)
+
+    def test_basic_branch_line(self):
+        wt = self.make_branch_and_tree('.')
+        wt.commit('a', rev_id='rev-a')
+        wt.commit('b', rev_id='rev-b')
+        wt.pull(wt.branch, True, 'rev-a')
+        wt.commit('c', rev_id='rev-c')
+        wt.pull(wt.branch, True, 'rev-b')
+        wt.set_parent_ids(['rev-b', 'rev-c'])
+        wt.commit('d', rev_id='rev-d')
+        
+        bi = loggraphprovider.BranchInfo('', wt, wt.branch)
+        gp = loggraphprovider.LogGraphProvider([bi], bi, False)
+        gp.load()
+        
+        state = loggraphprovider.GraphProviderFilterState(gp)
+        computed = gp.compute_graph_lines(state)
+        
+        # only mainline.
+        self.assertComputed(
+            [('rev-d', 0, False, [(0, 0, 0, True)]),
+             ('rev-b', 0, None,  [(0, 0, 0, True)]),
+             ('rev-a', 0, None,  []),],
+            computed)
+        
+        state.collapse_expand_rev(computed.filtered_revs[0])
+        computed = gp.compute_graph_lines(state)
+        
+        # expanded branch line.
+        self.assertComputed(
+            [('rev-d', 0, True, [(0, 0, 0, True), (0, 1, 2, True)]),
+             ('rev-c', 1, None, [(0, 0, 0, True), (1, 1, 0, True)]),
+             ('rev-b', 0, None, [(0, 0, 0, True), (1, 0, 0, True)]),
+             ('rev-a', 0, None, [])],
+            computed)
+        
