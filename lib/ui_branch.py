7f1713423d200decd2bef30e68a77993591adfa7
Timothy Reaves <treaves@silverfieldstech.com>
2011-03-23 17:07:59 -0400

    Changed fieldGrowthPolicy to AllNonFixedFieldsGrow.

--- lib/ui_branch.py
+++ lib/ui_branch.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'ui/branch.ui'
 #
-# Created: Wed Mar 23 10:39:16 2011
-#      by: PyQt4 UI code generator 4.7.2
+# Created: Wed Mar 23 17:07:09 2011
+#      by: PyQt4 UI code generator 4.8.2
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -11,22 +11,27 @@ from PyQt4 import QtCore, QtGui
 from bzrlib.plugins.qbzr.lib.i18n import gettext
 
 
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
 class Ui_BranchForm(object):
     def setupUi(self, BranchForm):
-        BranchForm.setObjectName("BranchForm")
+        BranchForm.setObjectName(_fromUtf8("BranchForm"))
         BranchForm.resize(349, 245)
         self.verticalLayout = QtGui.QVBoxLayout(BranchForm)
-        self.verticalLayout.setObjectName("verticalLayout")
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
         self.groupBox = QtGui.QGroupBox(BranchForm)
-        self.groupBox.setObjectName("groupBox")
+        self.groupBox.setObjectName(_fromUtf8("groupBox"))
         self.formLayout = QtGui.QFormLayout(self.groupBox)
-        self.formLayout.setFieldGrowthPolicy(QtGui.QFormLayout.ExpandingFieldsGrow)
-        self.formLayout.setObjectName("formLayout")
+        self.formLayout.setFieldGrowthPolicy(QtGui.QFormLayout.AllNonFixedFieldsGrow)
+        self.formLayout.setObjectName(_fromUtf8("formLayout"))
         self.from_label = QtGui.QLabel(self.groupBox)
-        self.from_label.setObjectName("from_label")
+        self.from_label.setObjectName(_fromUtf8("from_label"))
         self.formLayout.setWidget(0, QtGui.QFormLayout.LabelRole, self.from_label)
         self.horizontalLayout_2 = QtGui.QHBoxLayout()
-        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
         self.from_location = QtGui.QComboBox(self.groupBox)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
@@ -35,17 +40,18 @@ class Ui_BranchForm(object):
         self.from_location.setSizePolicy(sizePolicy)
         self.from_location.setEditable(True)
         self.from_location.setSizeAdjustPolicy(QtGui.QComboBox.AdjustToContentsOnFirstShow)
-        self.from_location.setObjectName("from_location")
+        self.from_location.setObjectName(_fromUtf8("from_location"))
         self.horizontalLayout_2.addWidget(self.from_location)
         self.from_picker = QtGui.QPushButton(self.groupBox)
-        self.from_picker.setObjectName("from_picker")
+        self.from_picker.setObjectName(_fromUtf8("from_picker"))
         self.horizontalLayout_2.addWidget(self.from_picker)
+        self.horizontalLayout_2.setStretch(0, 1)
         self.formLayout.setLayout(0, QtGui.QFormLayout.FieldRole, self.horizontalLayout_2)
         self.to_label = QtGui.QLabel(self.groupBox)
-        self.to_label.setObjectName("to_label")
+        self.to_label.setObjectName(_fromUtf8("to_label"))
         self.formLayout.setWidget(1, QtGui.QFormLayout.LabelRole, self.to_label)
         self.horizontalLayout_3 = QtGui.QHBoxLayout()
-        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.horizontalLayout_3.setObjectName(_fromUtf8("horizontalLayout_3"))
         self.to_location = QtGui.QComboBox(self.groupBox)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
@@ -54,27 +60,28 @@ class Ui_BranchForm(object):
         self.to_location.setSizePolicy(sizePolicy)
         self.to_location.setEditable(True)
         self.to_location.setSizeAdjustPolicy(QtGui.QComboBox.AdjustToContentsOnFirstShow)
-        self.to_location.setObjectName("to_location")
+        self.to_location.setObjectName(_fromUtf8("to_location"))
         self.horizontalLayout_3.addWidget(self.to_location)
         self.to_picker = QtGui.QPushButton(self.groupBox)
-        self.to_picker.setObjectName("to_picker")
+        self.to_picker.setObjectName(_fromUtf8("to_picker"))
         self.horizontalLayout_3.addWidget(self.to_picker)
+        self.horizontalLayout_3.setStretch(0, 1)
         self.formLayout.setLayout(1, QtGui.QFormLayout.FieldRole, self.horizontalLayout_3)
         self.verticalLayout.addWidget(self.groupBox)
         self.groupBox_2 = QtGui.QGroupBox(BranchForm)
-        self.groupBox_2.setObjectName("groupBox_2")
+        self.groupBox_2.setObjectName(_fromUtf8("groupBox_2"))
         self.verticalLayout_2 = QtGui.QVBoxLayout(self.groupBox_2)
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
         self.bind = QtGui.QCheckBox(self.groupBox_2)
-        self.bind.setObjectName("bind")
+        self.bind.setObjectName(_fromUtf8("bind"))
         self.verticalLayout_2.addWidget(self.bind)
         self.horizontalLayout = QtGui.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
         self.revision_label = QtGui.QLabel(self.groupBox_2)
-        self.revision_label.setObjectName("revision_label")
+        self.revision_label.setObjectName(_fromUtf8("revision_label"))
         self.horizontalLayout.addWidget(self.revision_label)
         self.revision = QtGui.QLineEdit(self.groupBox_2)
-        self.revision.setObjectName("revision")
+        self.revision.setObjectName(_fromUtf8("revision"))
         self.horizontalLayout.addWidget(self.revision)
         spacerItem = QtGui.QSpacerItem(78, 37, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem)
@@ -85,7 +92,7 @@ class Ui_BranchForm(object):
         self.revision_label.setBuddy(self.revision)
 
         self.retranslateUi(BranchForm)
-        QtCore.QObject.connect(BranchForm, QtCore.SIGNAL("disableUi(bool)"), self.groupBox.setDisabled)
+        QtCore.QObject.connect(BranchForm, QtCore.SIGNAL(_fromUtf8("disableUi(bool)")), self.groupBox.setDisabled)
         QtCore.QMetaObject.connectSlotsByName(BranchForm)
 
     def retranslateUi(self, BranchForm):
