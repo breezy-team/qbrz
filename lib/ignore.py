cdd676d8e1d6d0a40afee0e3fb2287b22a0d89bf
Alexander Belchenko <bialix@ukr.net>
2012-02-28 13:24:46 +0200

    qignore: use show_error in validate

--- lib/ignore.py
+++ lib/ignore.py
@@ -223,9 +223,7 @@ class IgnoreWindow(SubProcessDialog):
     def validate(self):
         patterns = self._collect_patterns()
         if not patterns:
-            QtGui.QMessageBox.critical(self,
-                gettext("Cancelled"),
-                gettext("No action selected"))
+            self.show_error(gettext("No action selected"))
             return False
         self.args = ['ignore'] + patterns
         self.process_widget.force_passing_args_via_file = True
