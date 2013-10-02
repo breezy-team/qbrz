64d10566e3056a816fd6029a23e79a55a1177c43
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2013-10-02 02:05:27 +0200

    qshelve: Add new command line option "-m" to qshelve.

--- lib/commands.py
+++ lib/commands.py
@@ -1114,6 +1114,7 @@ class cmd_qshelve(QBzrCommand):
         ui_mode_option,
         bzr_option('shelve', 'list'),
         bzr_option('shelve', 'directory'),
+        bzr_option('shelve', 'message'),
         Option('all', help='Select all changes.'),
         Option('complete', help='Show complete files.'),
         Option('ignore-whitespace', short_name='w',
@@ -1123,14 +1124,15 @@ class cmd_qshelve(QBzrCommand):
         ]
 
     def _qbzr_run(self, file_list=None, list=False, all=False, directory=None, ui_mode=False, 
-                            complete=False, ignore_whitespace=False, encoding=None):
+                            complete=False, ignore_whitespace=False, encoding=None, message=None):
         if list:
             initial_tab = 1
         else:
             initial_tab = 0
         self.main_window = ShelveWindow(file_list=file_list, directory=directory, ui_mode=ui_mode,
                                 select_all=all, initial_tab=initial_tab, complete=complete, 
-                                ignore_whitespace=ignore_whitespace, encoding=encoding)
+                                ignore_whitespace=ignore_whitespace, encoding=encoding,
+                                message=message)
         self.main_window.show()
         self._application.exec_()
 
