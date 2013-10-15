9790bd8fd7933615070f11e1374a59ddebdf273f
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2013-10-15 14:55:20 +0200

    Move parameters to the end to address review comment by Alexander Belchenko (bialix)

--- lib/commands.py
+++ lib/commands.py
@@ -1122,15 +1122,16 @@ class cmd_qshelve(QBzrCommand):
                help='Encoding of files content (default: utf-8).'),
         ]
 
-    def _qbzr_run(self, file_list=None, list=False, all=False, directory=None, ui_mode=False, 
-                            complete=False, ignore_whitespace=False, encoding=None):
+    def _qbzr_run(self, file_list=None, list=False, directory=None, ui_mode=False, 
+                            complete=False, ignore_whitespace=False, encoding=None, all=False):
         if list:
             initial_tab = 1
         else:
             initial_tab = 0
         self.main_window = ShelveWindow(file_list=file_list, directory=directory, ui_mode=ui_mode,
-                                select_all=all, initial_tab=initial_tab, complete=complete, 
-                                ignore_whitespace=ignore_whitespace, encoding=encoding)
+                                initial_tab=initial_tab, complete=complete, 
+                                ignore_whitespace=ignore_whitespace, encoding=encoding,
+                                select_all=all)
         self.main_window.show()
         self._application.exec_()
 
